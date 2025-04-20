efb516d7e809c1bba692ee8559b4706410e796cd
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-04-20 18:17:50 +0530

    Add files via upload

---
+++ Servlets/Database Application/Connection Pooling/src/src/DisplayServlet.java
@@ -0,0 +1,63 @@
+package com.anisha.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class DisplayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public DisplayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		try {
+		/*  Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@Localhost:1521:xe","system","anisha");
+		*/
+			
+			Connection connection = Connutils.datasource.getConnection();
+		Statement statement = connection.createStatement();
+		
+		ResultSet resultSet = statement.executeQuery("select * from Customer");
+		writer.print("<table border='1'>");
+	    writer.print("<tr><th>Firstname</th><th>Lastname</th><th>email</th><th>Mobile No</th><th>Addrees</th></tr>");
+		while(resultSet.next()) {
+		    writer.print("<tr>");
+		    writer.print("<td>" + resultSet.getString(1) + "</td>");
+		    writer.print("<td>" + resultSet.getString(2) + "</td>");
+		    writer.print("<td>" + resultSet.getString(3) + "</td>");
+		    writer.print("<td>" + resultSet.getString(4) + "</td>");
+		    writer.print("<td>" + resultSet.getString(5) + "</td>");
+		    writer.print("</tr>");
+		}
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+			writer.print("Error "+e.getMessage());
+		}
+		
+		writer.print("<a href='Customer.html'>Click here to add records...</a>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
